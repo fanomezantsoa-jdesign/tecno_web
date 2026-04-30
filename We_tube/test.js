65f50bffb40f7dbdbb90f1b7df3fdc6307779595
fanomezantsoa-jdesign <fanomezantsoaramarosandratana@gmail.com>
2026-04-30 21:51:41 +0300

    ajout de javascript

---
+++ We_tube/test.js
@@ -0,0 +1,12 @@
+const videos = document.querySelectorAll(".preview-video");
+
+videos.forEach(video => {
+  video.addEventListener("mouseenter", () => {
+    video.play();
+  });
+
+  video.addEventListener("mouseleave", () => {
+    video.pause();
+    video.currentTime = 0; // remet au début
+  });
+});
